54d85ee89322939d8f44f43515e1efa35203092a
Berk Karabacak <berk.karabacak@veniture.com.tr>
2021-01-03 12:04:58 +0300

    first commit

---
+++ src/main/java/com/veniture/rest/rest.java
@@ -0,0 +1,42 @@
+package com.veniture.rest;
+
+
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+import com.atlassian.sal.api.net.RequestFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+@Path("/main")
+public class rest {
+    @JiraImport
+    private RequestFactory requestFactory;
+    //private ApplicationProperties applicationProperties;
+    @JiraImport
+    private SearchService searchService;
+    @JiraImport
+    private JiraAuthenticationContext authenticationContext;
+    @JiraImport
+    private IssueManager issueManager;
+
+
+    public rest(RequestFactory requestFactory, SearchService searchService, JiraAuthenticationContext authenticationContext){
+        this.requestFactory = requestFactory;
+        this.issueManager= ComponentAccessor.getIssueManager();
+        this.searchService = searchService;
+        this.authenticationContext = authenticationContext;
+    }
+
+    @GET
+    @Path("/child")
+    public String child(@Context HttpServletRequest req, @Context HttpServletResponse resp)  {
+        return "dollarrate";
+    }
+}
---
+++ src/main/resources/js/changeButtonName.js
@@ -0,0 +1,2 @@
+//This is how you get an element with CSS selector, do some js to change text
+AJS.$("cssselector here");
