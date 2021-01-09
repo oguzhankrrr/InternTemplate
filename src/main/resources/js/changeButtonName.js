10551e5e4ff4d5904d6a56a8c8497b81526ba7b1
oguzhankrrr <oguzhankr@outlook.com>
2021-01-09 13:08:02 +0300

    Create button changed

--- src/main/resources/js/changeButtonName.js
+++ src/main/resources/js/changeButtonName.js
@@ -1,2 +1,3 @@
 //This is how you get an element with CSS selector, do some js to change text
-AJS.$("cssselector here");
+AJS.$("#create_link").css("background-color", "gray");
+AJS.$("#create_link").text("Degistirildi");
